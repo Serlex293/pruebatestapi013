e218445bfe43a3e969f16be1923e3ada16f98b76
Serlex293 <101515521+Serlex293@users.noreply.github.com>
2022-03-15 10:48:19 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-    date_default_timezone_set("Asia/Jakarta");
+    date_default_timezone_set("Perú/Lima");
     //Data From Webhook
     $content = file_get_contents("php://input");
     $update = json_decode($content, true);
