0b7dc2ba04f224a34cf58b55c08c2f04e5658786
Serlex293 <101515521+Serlex293@users.noreply.github.com>
2022-03-17 01:47:37 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -68,7 +68,7 @@ if(strpos($message, ".bin") === 0){
     if ($result1 == true) {
     send_message($chat_id,$message_id,"***VALID BIN✅
 ┏━━━━━━━━━━━━━━━━━━
-┠⌬ BIN:*** <code>$bin</code>
+┠⌬ BIN:*** "$bin"
 ***┠⌬ Brand: $brand
 ┠⌬ Level: $level
 ┠⌬ Bank: $bank
