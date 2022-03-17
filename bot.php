dffddb1f108a4e09c64780d95a6a88e111ca7e70
Serlex293 <101515521+Serlex293@users.noreply.github.com>
2022-03-17 09:55:58 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -66,16 +66,16 @@ if(strpos($message, ".bin") === 0){
             }
             
     if ($result1 == true) {
-    send_message($chat_id,$message_id,"VALID BIN✅
+    send_message($chat_id,$message_id,"**VALID BIN✅
 ┏━━━━━━━━━━━━━━━━━━
-┠⌬ BIN: ~~$bin~~
+┠⌬ BIN: $bin
 ┠⌬ Brand: $brand
 ┠⌬ Level: $level
 ┠⌬ Bank: $bank
 ┠⌬ Country: $country $flag
 ┠⌬ Type: $type
 ┗━━━━━━━━━━━━━━━━━━
- 〄Checked By: @$username");
+ 〄Checked By: @$username**");
     }
 else {
     send_message($chat_id,$message_id, "***Oye negro, métete tu bin por el culo no me sirve❌***");
